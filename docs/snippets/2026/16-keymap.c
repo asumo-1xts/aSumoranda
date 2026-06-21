8c3819d601a2942125fc03904d2f0bce5812dcaf
aSumo <1xtelescope@gmail.com>
2026-06-22 03:12:14 +0900

    docs: Edit an article

--- docs/snippets/2026/16-keymap.c
+++ docs/snippets/2026/16-keymap.c
@@ -1,51 +1,14 @@
 #include QMK_KEYBOARD_H
 #include "keymap.h"
 
-// Magic機能を使わないので、容量を削減するため上書きする
-uint16_t keycode_config(uint16_t keycode) { return keycode; }
-uint8_t mod_config(uint8_t mod) { return mod; }
-
 /**
  * ==============================
- * 内部関数の宣言
+ * 内部変数・内部関数の定義
  * ==============================
  */
 
 static uint32_t key_timer;
 bool is_rgb_timeout = false;
-void yawn(uint8_t hue, uint8_t sat, uint8_t val);
-void check_rgb_timeout(void);
-void refresh_rgb(void);
-
-/**
- * ==============================
- * QMKで用意されているAPI
- * ==============================
- */
-
-void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom();
-  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-  yawn(HUE, SAT, VAL);
-}
-
-void housekeeping_task_user(void) {
-#ifdef RGBLIGHT_TIMEOUT
-  check_rgb_timeout();
-#endif
-}
-
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef RGBLIGHT_TIMEOUT
-  if (record->event.pressed) refresh_rgb();
-#endif
-}
-
-/**
- * ==============================
- * 内部関数の定義
- * ==============================
- */
 
 void yawn(uint8_t hue, uint8_t sat, uint8_t val) {
   for (int16_t i = 0; i <= RGBLIGHT_LIMIT_VAL; i += RGBLIGHT_VAL_STEP) {
@@ -62,17 +25,66 @@ void yawn(uint8_t hue, uint8_t sat, uint8_t val) {
   rgblight_sethsv_noeeprom(hue, sat, val);  // 最終的な明るさ
 }
 
+void check_rgb_timeout(void) {
+  if (!is_rgb_timeout && (timer_elapsed32(key_timer) > RGBLIGHT_TIMEOUT)) {
+    is_rgb_timeout = true;
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_TWINKLE);
+    rgblight_suspend();
+  }
+}
+
 void refresh_rgb(void) {
   key_timer = timer_read32();
   if (is_rgb_timeout) {
     is_rgb_timeout = false;
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
     rgblight_wakeup();
   }
 }
 
-void check_rgb_timeout(void) {
-  if (!is_rgb_timeout && timer_elapsed32(key_timer) > RGBLIGHT_TIMEOUT) {
-    rgblight_suspend();
-    is_rgb_timeout = true;
-  }
+/**
+ * ==============================
+ * QMKで用意されているAPI
+ * ==============================
+ */
+
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom();
+  yawn(HUE, SAT, VAL);
 }
+
+void housekeeping_task_user(void) {
+#ifdef RGBLIGHT_TIMEOUT
+  check_rgb_timeout();
+#endif
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef RGBLIGHT_TIMEOUT
+  if (record->event.pressed) {
+    refresh_rgb();
+  }
+#endif
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch (get_highest_layer(state)) {
+    case _MI:  // MIDI layer
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
+      break;
+    default:  // any other layers
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+      break;
+  }
+  return state;
+}
+
+/**
+ * ==============================
+ * ファームウェア容量削減
+ * ==============================
+ */
+
+// Magic機能は使わないので上書き
+uint16_t keycode_config(uint16_t keycode) { return keycode; }
+uint8_t mod_config(uint8_t mod) { return mod; }
--- docs/snippets/2026/16-keymap.h
+++ docs/snippets/2026/16-keymap.h
@@ -1,37 +1,67 @@
-#define TTTT KC_TRNS
+/**
+ * ==============================
+ * RGBライトのカラー設定: HSV_CORAL
+ * ==============================
+ */
 
-// HSV_CORAL
-static const uint8_t HUE = 11;
-static const uint8_t SAT = 176;
-static const uint8_t VAL = 127;
+const uint8_t HUE = 11;
+const uint8_t SAT = 176;
+const uint8_t VAL = 127;
+const uint8_t RGBLED_RAINBOW_SWIRL_INTERVALS[] PROGMEM = {30};
+
+/**
+ * ==============================
+ * キーマップの定義
+ * ==============================
+ */
+
+#define _BS 0
+#define _FN 1
+#define _MI 2
+#define TTTT KC_TRNS
+#define NNNN KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        MO(1),  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
-        KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_PSCR,KC_NO,  KC_NO,
+    [_BS] = LAYOUT(
+        MO(_FN),KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
+        KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_PSCR,NNNN   ,NNNN,
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
-        KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_GRV, KC_BSPC,KC_NO,
+        KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_GRV, KC_BSPC,KC_HOME,
                 KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
-        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_ENT, KC_NO,
+        KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_ENT, KC_END,
                 KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
-        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,        KC_HOME,
+        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_BSLS,        MS_WHLU,
                 KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,
-        KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  KC_END,
-                MO(1),  KC_LWIN,KC_LALT,KC_LNG1,KC_SPC,
-                KC_SPC, KC_LNG2,KC_RWIN,MO(1),  KC_LEFT,        KC_DOWN,KC_RGHT
+        KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,        KC_UP,  MS_WHLD,
+                MS_BTN1,KC_LWIN,KC_LALT,KC_LNG1,KC_SPC,
+                KC_SPC, KC_LNG2,KC_RWIN,MO(_FN),KC_LEFT,        KC_DOWN,KC_RGHT
     ),
-    [1] = LAYOUT(
+    [_FN] = LAYOUT(
         TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
         TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
         TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
         TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   KC_DEL, TTTT,
-                TTTT,   TTTT,   KC_UP,  TTTT,   TTTT,   TTTT,
+                TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
         TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
-                KC_DEL, KC_LEFT,KC_DOWN,KC_RGHT,TTTT,   TTTT,
-        TTTT,   TTTT,   TTTT,   UG_VALU,TTTT,   TTTT,   TTTT,           TTTT,
-                KC_ENT, TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
-        TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   KC_CAPS,        TTTT,   TTTT,
+                TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
+        TTTT,   TTTT,   TTTT,   UG_VALU,TTTT,   TTTT,   TTTT,           MS_BTN3,
+                KC_DEL, TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
+        TTTT,   TG(_MI),TTTT,   TTTT,   TTTT,   KC_CAPS,        MS_UP,  MS_BTN4,
+                MS_BTN2,TTTT,   TTTT,   TTTT,   TTTT,
+                TTTT,   TTTT,   TTTT,   TTTT,   MS_LEFT,        MS_DOWN,MS_RGHT
+    ),
+    [_MI] = LAYOUT(
+        TTTT,   MI_CH1, MI_CH2, MI_CH3, MI_CH4, MI_CH5, MI_CH6,
+        MI_CH7, MI_CH8, MI_CH9, MI_CH10,MI_CH11,MI_CH12,TTTT,   TTTT,   TTTT,
+        TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
+        TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   MI_OCTU,
+                TTTT,   TTTT,   MI_Cs,  MI_Ds,  TTTT,   MI_Fs,
+        MI_Gs,  MI_As,  TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   MI_OCTD,
+                TTTT,   MI_C,   MI_D,   MI_E,   MI_F,   MI_G,
+        MI_A,   MI_B,   MI_C,   TTTT,   TTTT,   TTTT,   TTTT,           MI_VELU,
+                TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
+        TTTT,   TTTT,   TTTT,   TTTT,   TTTT,   TTTT,           MI_MODU,MI_VELD,
                 TTTT,   TTTT,   TTTT,   TTTT,   TTTT,
-                TTTT,   TTTT,   TTTT,   TTTT,   TTTT,           TTTT,   TTTT
+                TTTT,   TTTT,   TTTT,   TTTT,   MI_BNDD,        MI_MODD,MI_BNDU
     )
 };
