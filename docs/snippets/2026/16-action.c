7d74e4b5747d8fe68c055e20c15256566e73810f
aSumo-1xts <1xtelescope@gmail.com>
2026-08-11 14:10:48 +0900

    docs: Edit an article

---
+++ docs/snippets/2026/16-action.c
@@ -0,0 +1,5 @@
+// 他の部分はそのまま残す
+
+#ifdef FLOW_TAP_TERM // [!code --]
+    flow_tap_update_last_event(record); // [!code --]
+#endif FLOW_TAP_TERM // [!code --]
--- docs/snippets/2026/17-action.c
+++
@@ -1,8 +0,0 @@
-// 他の部分はそのまま残す
-
-#ifdef FLOW_TAP_TERM // [!code --]
-    flow_tap_update_last_event(record); // [!code --]
-#endif // FLOW_TAP_TERM // [!code --]
-// #ifdef FLOW_TAP_TERM // [!code ++]
-//     flow_tap_update_last_event(record); // [!code ++]
-// #endif // FLOW_TAP_TERM // [!code ++]
